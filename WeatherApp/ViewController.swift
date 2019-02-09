d117d2f43f82794707d39bab0d1057a0395fe9bf
Олег Озимок <Oleg.Tukavin@gmail.com>
2019-02-09 19:22:58 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Oleg Ozimok on 09/02/2019.
+//  Copyright © 2019 Oleg Ozimok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
